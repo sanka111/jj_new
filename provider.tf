3477cbe01edbc0c9ee796b4043815b36e014550c
Sanka <galappaththi111@gmail.com>
2024-09-22 01:55:53 +0530

    update version

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.7.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "=> 5.55"
      }
   }
 }
