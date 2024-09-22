aeb77e6011bd1c35eacfdad524fec47cbc3211b8
Sanka <galappaththi111@gmail.com>
2024-09-22 16:05:11 +0530

    change region

--- generic-variables.tf
+++ generic-variables.tf
@@ -3,7 +3,7 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "ap-southeast-1"  
+  default = "eu-north-1"  
 }
 # Environment Variable
 variable "environment" {
