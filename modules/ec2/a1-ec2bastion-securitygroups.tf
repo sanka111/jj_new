ce94375dc34decd6b8bca9d71d2dff33d2fa7ebc
Sanka <galappaththi111@gmail.com>
2024-09-22 16:42:06 +0530

    allow any bastion

--- modules/ec2/a1-ec2bastion-securitygroups.tf
+++ modules/ec2/a1-ec2bastion-securitygroups.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [var.cidr_blocks]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -23,12 +23,3 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-
-resource "aws_security_group_rule" "allow_ssh_public" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = aws_security_group.bastion_sg.id
-  cidr_blocks = ["0.0.0.0/0"]
-}
