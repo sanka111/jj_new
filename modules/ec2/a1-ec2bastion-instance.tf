a5b28e87151c3be3ddc105d6dc1409e38b967e52
Sanka <galappaththi111@gmail.com>
2024-09-22 12:16:28 +0530

    install git, aws cli and terraform to bastion

--- modules/ec2/a1-ec2bastion-instance.tf
+++ modules/ec2/a1-ec2bastion-instance.tf
@@ -12,4 +12,22 @@ module "ec2_bastion" {
   subnet_id              = var.dmz_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   tags = var.common_tags
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y git
+
+    # Install AWS CLI v2
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    aws --version
+
+    # Install Terraform
+    sudo yum install -y yum-utils
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+    sudo yum -y install terraform
+    terraform -version
+  EOF
 }
